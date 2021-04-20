3068dcff6e5c2cac192cff024278bbe56382eb4d
hendrikvg <63549321+hendrikvg@users.noreply.github.com>
2021-04-20 12:29:01 +0200

    Entry.h & Entry.cpp

--- Entry.cpp
+++ Entry.cpp
@@ -1,15 +1,39 @@
+/*
+==============================================================
+ Filename    :  Entry.cpp
+ Authors     :  Hendrik van Gils    (s1920677)  h.vangils@student.utwente.nl
+                Deniz Ugurlu        (s1797735)  d.a.ugurlu@student.utwente.nl
+ Version     :  1.0
+ License     :  none.
+ Description :  The aim of this file is to create a wrapper class that can be either
+                a constant or a lambda function.
+==============================================================
+*/
+
 #include "Entry.h"
 
+/// <summary>
+/// Constructor that initializes with a lambda function.
+/// </summary>
+/// <param name="entryFunc">Lambda function to initialize with.</param>
 Entry::Entry(std::function<double()> entryFunc) : entryFunction(entryFunc) {
     isConstant = false;
 }
 
+/// <summary>
+/// Constructor that initializes with a constant.
+/// </summary>
+/// <param name="constant">Constant to initialize with.</param>
 Entry::Entry(double constant) : constant(constant) {
     isConstant = true;
 }
 
+/// <summary>
+/// Returns either a constant or the result of a lambda function.
+/// </summary>
+/// <returns></returns>
 double Entry::calculate() {
-    return isConstant ? constant : entryFunction();
+    return isConstant ? constant : entryFunction(); // Conditional ternary operator..
 }
 
 double Entry::operator-(Entry B)
--- Entry.h
+++ Entry.h
@@ -1,46 +1,59 @@
+/*
+==============================================================
+ Filename    :  Entry.h
+ Authors     :  Hendrik van Gils    (s1920677)  h.vangils@student.utwente.nl
+                Deniz Ugurlu        (s1797735)  d.a.ugurlu@student.utwente.nl
+ Version     :  1.0
+ License     :  none.
+ Description :  The aim of this file is to create a wrapper class that can be either
+                a constant or a lambda function.
+==============================================================
+*/
+
 #ifndef SRC_ENTRY_H
 #pragma once
 #define SRC_ENTRY_H
 
-#include <functional>
-
-
-
+#include <functional> // For functions as inputs.
 
 /// <summary>
-/// This class is needed since we want to be able to fill in variables and constants into our matrix class. This cannot be done without a wrapper class that checks whether the input is
-/// variable or constant.
+/// This class is needed since we want to be able to fill in variables and constants 
+/// into our matrix class. This cannot be done without a wrapper class that checks 
+/// whether the input is a variable or constant.
 /// </summary>
 class Entry {
+private:
+
+    double constant;
+    std::function<double()> entryFunction;
+    bool isConstant; // Tells whether it should return a function or constant.
 
 public:
 
     /// <summary>
-    /// constructor that takes variables
+    /// Constructor that takes lambda function.
     /// </summary>
-    /// <param name="entryFunc">enter variable</param>
+    /// <param name="entryFunc">Enter lambda function.</param>
     Entry(std::function<double()> entryFunc);
 
     /// <summary>
-    /// constructor that takes constants
+    /// Constructor that takes constants.
     /// </summary>
-    /// <param name="constant">constants</param>
+    /// <param name="constant">Constants of type double.</param>
     Entry(double constant);
 
     /// <summary>
-    /// returns the values inside object, by first checking wether it is constant or variable
+    /// Returns the values inside object, by first checking wether it is constant or 
+    /// variable.
     /// </summary>
     double calculate();
+
+    /// Operator overrides that enable adding and subtracting with other Entry
+    /// objects and numbers of type double.
     double operator-(Entry B);
     double operator-(double B);
     double operator+(Entry B);
     double operator+(double B);
-
-private:
-    double constant;
-    std::function<double()> entryFunction;
-
-    bool isConstant;
 };
 
 
